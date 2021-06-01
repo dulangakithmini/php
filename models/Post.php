3c7d18a394a559c848ccd06beced43f318449f2e
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-06-02 04:07:21 +0530

    Define properties of post

--- models/Post.php
+++ models/Post.php
@@ -5,4 +5,13 @@ class Post
 {
     private $conn;
     private $table = 'posts';
+
+    //Post properties
+    public $id;
+    public $category_id;
+    public $category_name;
+    public $title;
+    public $body;
+    public $author;
+    public $created_at;
 }
