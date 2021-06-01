6d71e726e2a863a1f5f90b549b6daaffebe27334
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-06-02 03:25:47 +0530

    Create db parameters

--- config/Database.php
+++ config/Database.php
@@ -3,5 +3,10 @@
 
 class Database
 {
-
+    //DB parameters
+    private $host = "localhost";
+    private $db_name = "myblog";
+    private $username = "root";
+    private $password = "";
+    private $conn;
 }
