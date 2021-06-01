a0588a3625ba7832e377e9dabaadee0139ce2ff4
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-06-02 03:58:56 +0530

    Set error mode

--- config/Database.php
+++ config/Database.php
@@ -17,6 +17,7 @@ class Database
 
         try {
             $this->conn = new PDO("mysql: host " . $this->host . ";dbname" . $this->db_name, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $exception) {
             echo "Connection Error: " . $exception->getMessage();
         }
