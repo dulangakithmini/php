52f595a25a008f8127db4c18d7dc2323e2437efc
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-06-02 03:56:33 +0530

    Create new PDO

--- config/Database.php
+++ config/Database.php
@@ -9,4 +9,16 @@ class Database
     private $username = "root";
     private $password = "";
     private $conn;
+
+    //DB connection
+    public function connect()
+    {
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO("mysql: host " . $this->host . ";dbname" . $this->db_name, $this->username, $this->password);
+        } catch (PDOException $exception) {
+            echo "Connection Error: " . $exception->getMessage();
+        }
+    }
 }
